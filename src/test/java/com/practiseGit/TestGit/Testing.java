da98ccfe408c54378cd119fcf2974fff14ae62ac
hasneetnehra <hasneet.nehra@nagarro.com>
2019-12-07 10:53:28 +0530

    one more

--- src/test/java/com/practiseGit/TestGit/Testing.java
+++ src/test/java/com/practiseGit/TestGit/Testing.java
@@ -6,6 +6,15 @@ public class Testing {
 	System.out.println("Web automation");
 	System.out.println("Mobile");
 	
+	System.out.println("Develop");
+	
+	System.out.println("One more change");
+	
+	int a= 10;
+	int b=20;
+	
+	System.out.println(a+b);
+	
 
 	}
 
