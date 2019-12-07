50d68374118c0493674926fbf04f64f52f524261
hasneetnehra <hasneet.nehra@nagarro.com>
2019-12-07 17:00:52 +0530

    git

---
+++ src/test/java/com/practiseGit/TestGit/gitchecking.java
@@ -0,0 +1,13 @@
+package com.practiseGit.TestGit;
+
+public class gitchecking {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("checkk");
+		System.out.println("testt");
+
+	}
+
+}
