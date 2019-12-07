5b2ed638c45ea203ce1586f189c778e9253ac8ca
hasneetnehra <hasneet.nehra@nagarro.com>
2019-12-07 10:45:41 +0530

    ini

---
+++ src/test/java/com/practiseGit/TestGit/Testing.java
@@ -0,0 +1,12 @@
+package com.practiseGit.TestGit;
+
+public class Testing {
+
+	public static void main(String[] args) {
+	System.out.println("Web automation");
+	System.out.println("Mobile");
+	
+
+	}
+
+}
