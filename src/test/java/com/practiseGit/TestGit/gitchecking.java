1ae3159b03ffa4503d585893a649b0f7d4c707e5
hasneetnehra <hasneet.nehra@nagarro.com>
2019-12-08 10:30:00 +0530

    test

--- src/test/java/com/practiseGit/TestGit/gitchecking.java
+++ src/test/java/com/practiseGit/TestGit/gitchecking.java
@@ -1,13 +1,22 @@
 package com.practiseGit.TestGit;
 
+import org.junit.Test;
+
 public class gitchecking {
 
-	public static void main(String[] args) {
+	
 		
-		
+	@Test
+	public void firstTest() {
 		System.out.println("checkk");
 		System.out.println("testt");
-
+	}
+	
+	@Test
+	public void secondTest() {
+		System.out.println("checkk");
+		System.out.println("testt");
+	}
 	}
 
-}
+
